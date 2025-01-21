0e6363ed95b8548608817729ef5b12d44bf865d4
SebasC <sebastiancandelah@gmail.com>
2025-01-21 11:25:01 -0500

    nuevo dia

--- main.tf
+++ main.tf
@@ -2,12 +2,41 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_instance" "demo" {
+resource "aws_instance" "example" {
     ami = "ami-0c55b159cbfafe1f0"
-    instance_type = "t2.micro"  
+    instance_type = "t2.micro" 
+    vpc_security_group_ids = [aws_security_group.instance.id] 
+
+    user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+    user_data_replace_on_change = true
 
     tags = {
         Name = "terraform-example"
     }
 }
 
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port that the server will use for HTTP request"
+  type        = number
+  default     = 8080
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
+}
